fb9071b36472cd284d8ffcae6441ded18765df11
birme <jonas.birme@eyevinn.se>
2022-04-06 17:55:21 +0200

    fix: we need ice server config for player

--- app/src/watch.ts
+++ app/src/watch.ts
@@ -5,9 +5,23 @@ function base64decode(b64: string) {
   return buf.toString();
 }
 
+let iceServers: RTCIceServer[] = [{ urls: "stun:stun.l.google.com:19302" }];
+
+if (process.env.BC_ICE_SERVERS) {
+  iceServers = [];
+  process.env.BC_ICE_SERVERS.split(",").forEach(server => {
+    // turn:<username>:<password>@turn.eyevinn.technology:3478
+    const m = server.match(/^turn:(\S+):(\S+)@(\S+):(\d+)/);
+    if (m) {
+      const [ _, username, credential, host, port ] = m;
+      iceServers.push({ urls: "turn:" + host + ":" + port, username: username, credential: credential });
+    }
+  });
+}
+
 export async function watch(channelUrl, video) {
   if (channelUrl) {
-    const player = new WebRTCPlayer({ video: video, type: "se.eyevinn.webrtc" });
+    const player = new WebRTCPlayer({ video: video, type: "se.eyevinn.webrtc", iceServers: iceServers });
     await player.load(new URL(channelUrl));
   }
 }
